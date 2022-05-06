1783245c872d885f23405ccd1d8d75631c26f73e
Kimberly Arcena <kvarcena@up.edu.ph>
2022-05-07 01:07:02 +0800

    Update ButtonState

--- Arduino/buttonLEDTest/buttonLEDTest.ino
+++ Arduino/buttonLEDTest/buttonLEDTest.ino
@@ -4,12 +4,17 @@ int buttonPin = 2;
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int buttonState = digitalRead(buttonPin);
-  digitalWrite(ledPin, buttonState);
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
 
 }
